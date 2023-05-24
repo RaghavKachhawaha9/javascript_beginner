251fefd0944aae23e576c340aa4ded62c41ee118
RaghavKachhawaha9 <raghav.kachhawaha9@gmail.com>
2023-05-25 03:02:54 +0530

    added file

---
+++ 05_variables_js/index.js
@@ -0,0 +1,21 @@
+
+let name;
+console.log(name); //output -> undefined
+/* In javascript, by default the value of variable which we declare 
+is undefined
+*/
+
+let Name = 'John'; // we can declare string in both "" or '' quotes,generally in js single quotes are used.
+console.log(Name) //output -> John
+
+
+/***************************************************/
+
+//declaring multiple variables
+
+// 1. let firstName, lastName; // they both are undefined
+// 2. let firstName= 'John' , lastName= 'Wick';
+// 3. Modern best practice is to declare both the variables on separate lines
+
+let firstName= 'John';
+let lastName= 'Wick';
---
+++ 05_variables_js/read.md
@@ -0,0 +1,15 @@
+## Variable in JS
+*** 
+1. We use a variable to store data temporarily in computers memory.
+2. We store that data in memory and give it a name => variable name
+3. Using this name we can read the data in the future.
+4. Before ES6, **var** keyword was used to declare a variable. 
+5. After ES6, best practice is to use **let** keyword to declare a variable.
+***  
+
+### Rules for naming variables in javascript
+***
+1. Cannot be a reserved keyword.
+2. Should be a meaningful name. 
+3. Cannot start with a number. (1name)=> not allowed
+4. Cannot contain a space or a hyphen
