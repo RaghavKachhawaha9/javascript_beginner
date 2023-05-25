a2818c84528beccfe9bb0575c514c30f81ea3a94
RaghavKachhawaha9 <raghav.kachhawaha9@gmail.com>
2023-05-26 02:00:02 +0530

    added file

---
+++ 07_primitive_types/README.md
@@ -0,0 +1,14 @@
+## Primitive/value types vs Reference types
+***
+## Primitive types :
+___
+* String 
+* Number 
+* Boolean => true, false
+* Undefined
+* null
+  
+Note -> In ES6 we have another primitive as *symbol*
+
+___
+  
---
+++ 07_primitive_types/index.js
@@ -0,0 +1,13 @@
+
+let name = 'John'  // String Literal
+let age = 30 // Number Literal
+let isApproved =  true; //Boolean Literal
+let firstName; //undefined
+/* we can declare undefined also as 
+let firstName = undefined 
+but in contrast to above we have null */
+
+/* suppose in future you have to select a color
+   till then we will assign it as null
+*/
+let selectColor = null;
