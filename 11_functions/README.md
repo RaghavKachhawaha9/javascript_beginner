484fe8df00409066669d89db8c0c77283682b4de
RaghavKachhawaha9 <raghav.kachhawaha9@gmail.com>
2023-05-27 02:43:29 +0530

    added files

---
+++ 11_functions/README.md
@@ -0,0 +1,4 @@
+## Functions
+
+* Functions are one of the fundamental building blocks in javascript.
+* Functions are basically set of statements that performs a task.
---
+++ 11_functions/index.js
@@ -0,0 +1,44 @@
+
+function greet() {
+    //body of function 
+    console.log("Hello world")
+}
+
+greet(); // Hello world
+
+// argument and parameter
+
+function address(name) { //name is parameter here
+    console.log('hello ' + name)
+}
+
+address('John') // john is argument here  
+address('Mary') // we can reuse this function with different input
+
+//output 
+// hello John
+// hello Mary
+
+
+// We can have multiple parameters
+
+function welcome(name, lastName){
+    console.log('Hello ' + name + ' ' + lastName);
+}
+
+welcome('John', 'Wick') // output -> Hello John Wick
+
+
+//Types of function
+
+// return keyword
+
+function square(num){
+    return num*num;
+}
+
+let number = square(3)
+console.log(number) // 9
+//creating a variable to call a function is not necessary 
+console.log(square(2)) //4
+
